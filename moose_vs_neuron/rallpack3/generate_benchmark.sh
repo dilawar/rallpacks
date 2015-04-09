4340d7582b55b2e201e4094050bbf18ead815889
Dilawar Singh <dilawars@ncbs.res.in>
2015-04-09 23:43:14 +0530

    Added scripts to benchmark.

---
+++ moose_vs_neuron/rallpack3/generate_benchmark.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+# This script generates benchamark.
+set -x
+set -e
+echo "Run each benchmark 5 times"
+for i in `seq 500 500 35000`
+do
+    for j in `seq 1 1 3` 
+    do
+        python moose_sim.py --run_time 0.25 --ncomp $i
+        python neuron_sim.py --run_time 0.25 --ncomp $i
+    done
+done
